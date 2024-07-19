42652ea2feabaef0d20f7e8666a8277210053ba8
Bharath M <bharathh.xworkz@gmail.com>
2024-07-19 09:41:47 +0530

    Hello

---
+++ Biodata.java
@@ -0,0 +1,9 @@
+class Biodata{
+public static void main(String[] args){
+Info.Name("BHARATH M");
+Info.Age(22);
+Info.Location("Bengaluru");
+Info.School("St.Sophia Convent");
+Info.College("Atria Institute of Technology");
+}
+}
