a30c26f24175076e6d3bd092c49365cb2d9c06f5
Bharath M <bharathh.xworkz@gmail.com>
2024-07-20 15:09:19 +0530

    Java Source File

---
+++ PersonRunner.java
@@ -0,0 +1,13 @@
+class PersonRunner{
+public static void main(String[] args){
+if(args.length==3){
+ String name="Bharath";
+ String email="abc@gmail.com";
+ String age=22;
+ String password="abcd123";
+ }
+ else{
+	 System.out.println("Enter 4 args");
+ }
+ }
+ }
