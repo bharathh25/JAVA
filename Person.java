b52fbcd56ad1dda92339a405ae8ee934297f5c14
Bharath M <bharathh.xworkz@gmail.com>
2024-07-20 15:38:17 +0530

    Java Source files

--- Person.java
+++ Person.java
@@ -1,8 +1,8 @@
 class Person{
-	public static void info(String name,String email,int age, String password){
-		System.out..println("Name:"+name);
-		System.out..println("Email:"+email);
-		System.out..println("Age:"+age);
-		System.out..println("Password:"+password);
+	public static void info(String name,String email,String age, String password){
+		System.out.println("Name:"+name);
+		System.out.println("Email:"+email);
+		System.out.println("Age:"+age);
+		System.out.println("Password:"+password);
 	}
 }
--- PersonRunner.java
+++ PersonRunner.java
@@ -1,10 +1,13 @@
 class PersonRunner{
 public static void main(String[] args){
-if(args.length==3){
- String name="Bharath";
- String email="abc@gmail.com";
- String age=22;
- String password="abcd123";
+if(args.length==4){
+ String name=args[0];
+ String email=args[1];
+ String age=args[2];
+ String password=args[3];
+ 
+ 
+	Person.info(name,email,age,password);
  }
  else{
 	 System.out.println("Enter 4 args");
